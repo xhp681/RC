5c275ccdcbb8e4c9f4beea2c2ff065bb4ec4368b
Rince King <527883283@qq.com>
2021-01-28 16:48:50 +0800

    Rs.Common下的Utils.cs文件增加两函数；

---
+++ RC/Rs.Common/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs.Common
+{
+    public class Utils
+    {
+        /// <summary>
+        /// 系统绝对目录
+        /// </summary>
+        public static string RootDictionaryPath { get; set; } = AppContext.BaseDirectory;
+        /// <summary>
+        /// 系统虚拟目录
+        /// </summary>
+        /// <param name="virtualPath"></param>
+        /// <returns></returns>
+        public static string ContentRootMapPath(string virtualPath)
+        {
+            if (string.IsNullOrEmpty(virtualPath))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                var path = Path.DirectorySeparatorChar.ToString();
+                var altPath = Path.AltDirectorySeparatorChar.ToString();
+                if (!RootDictionaryPath.EndsWith(path) && !RootDictionaryPath.EndsWith(altPath))
+                {
+                    RootDictionaryPath += path;
+                }
+
+                if (virtualPath.StartsWith("~/"))
+                {
+                    return virtualPath.Replace("~/", RootDictionaryPath).Replace("/", path);
+                }
+                else
+                {
+                    return Path.Combine(RootDictionaryPath, virtualPath);
+                }
+            }
+        }
+    }
+}
