63ee1fa6ea6bd5d7a4d0b4b89f8f891d361648c0
Rince King <527883283@qq.com>
2021-02-14 20:31:34 +0800

    调通程序。显示页面有待完善。20210214

---
+++ RC/Rs.Config/Events/IEventPublisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs.Config
+{
+    public interface IEventPublisher
+    {
+        /// <summary>
+        /// Publish event to consumers
+        /// </summary>
+        /// <typeparam name="TEvent">Type of event</typeparam>
+        /// <param name="event">Event object</param>
+        Task PublishAsync<TEvent>(TEvent @event);
+    }
+}
---
+++ RC/Rs.Config/Seo/UrlRecord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs.Config.Seo
+{
+    public partial class UrlRecord:BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the entity identifier
+        /// </summary>
+        public int EntityId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity name
+        /// </summary>
+        public string EntityName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slug
+        /// </summary>
+        public string Slug { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value indicating whether the record is active
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the language identifier
+        /// </summary>
+        public int LanguageId { get; set; }
+    }
+}
